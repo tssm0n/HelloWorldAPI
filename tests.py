9004982ffaff471e4a0490ef46a05827e3fbea65
tss <tss@bigdata.831a.com>
2016-10-27 22:04:43 -0400

    Adding "tests"

---
+++ tests.py
@@ -0,0 +1,9 @@
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+
+    def test_stuff(self):
+        self.assertEqual('test','test')
+
+if __name__ == '__main__':
+    unittest.main()
